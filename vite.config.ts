12dba878d3fd2e3506cf039ff3b30452f44991fc
Kjell Magne Fauske <kjellmf@gmail.com>
2024-12-15 12:54:44 +0100

    Revert "Update vite.config.ts"
    This reverts commit 8ffe4b1877f9322dc0be31c39a10a358c89bda81.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: "/experiment-shortest-path/",
+  base: "/experiment-shortest-path/",
   plugins: [vue()],
   resolve: {
     alias: {
